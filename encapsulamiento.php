469b2ce8cc9da873722fe9b43e36baaa23972688
marino5710 <brianmarin4@gmail.com>
2023-05-04 10:54:53 -0600

    DEFINICION DE LOS ATRIBUTOS Y METODO CONSTRUCTOR

---
+++ encapsulamiento.php
@@ -0,0 +1,15 @@
+<?php
+
+class Producto {
+    #DEFINICION DE LOS ATRIBUTOS
+    protected string $nombre; 
+    public int $precio; 
+    public bool $disponible; 
+
+    #METODO CONSTRUCTOR, SE EJECUTA AL EFECTUAR LA INSTANCIA
+
+    
+}
+
+
+?>
