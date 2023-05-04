b3941d13479115f3796dd1ecaac998beac0a7779
marino5710 <brianmarin4@gmail.com>
2023-05-04 11:25:22 -0600

    se definieron los metodos y se utulizo metodo getter y setter

--- encapsulamiento.php
+++ encapsulamiento.php
@@ -16,8 +16,21 @@ class Producto {
         $this-> $disponible;
     }
 
-    
-}
+    #DEFINICION DE METODOS
+    public function mostrarProducto() {
+        echo "El Producto es: " . $this->nombre . " y su precio es de: " . $this->precio; 
+      }
+    #METODO GETTER PARA LA PROPIEDAD PROTEGIDA DE NOMBRE
+    public function getNombre() : string {
+        return $this->nombre; 
+    }
+    #METODO SETTER PARA LA PROPIEDAD PROTEGIDA DE NOMBRE
+    public function setNombre(string $nombre) {
+            $this->nombre = $nombre;
+    }
+ }
 
+ $producto = new Producto('Tablet', 200, true); 
+ //
 
 ?>
