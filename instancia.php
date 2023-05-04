293b747ec2bf5e295c6183c169245e9b4c00c55a
marino5710 <brianmarin4@gmail.com>
2023-05-04 10:04:28 -0600

    SE DEFINIERON LOS ATRIBUTOS

--- instancia.php
+++ instancia.php
@@ -1,5 +1,14 @@
 <?php
 
+class Producto {
+    #DEFINICIO DE LOS ATRIBUTOS
+    public string $nombre;
+    public int $precio; 
+    public bool $disponible; 
+
+    #metodo constructor, este se ejecuta al efectuar la instancia
+    
+}
 
 
 ?>
