e2d56fe55b915cd22d805638012937872156db1e
marino5710 <brianmarin4@gmail.com>
2023-05-04 12:09:59 -0600

    SE CREO LA CLASE BICICLETA

--- HERypOLIM.php
+++ HERypOLIM.php
@@ -19,5 +19,13 @@ class Transporte {
     }
 }
 
+class Bicicleta extends Transporte {
+
+        public function getInfo() : string {
+            return "el transporte tiene " . $this->ruedas . "ruedas y una capacidad de ". $this->capacidad . " personas y NO GASTA GASOLINA";
+        }
+}
+
+
 
 ?>
