23e860aa4c515a75e62520fe3e9004359602f461
marino5710 <brianmarin4@gmail.com>
2023-05-04 11:11:31 -0600

    se les da valor a los atributos

--- encapsulamiento.php
+++ encapsulamiento.php
@@ -7,6 +7,14 @@ class Producto {
     public bool $disponible; 
 
     #METODO CONSTRUCTOR, SE EJECUTA AL EFECTUAR LA INSTANCIA
+    public function __construct(string $nombre, int $precio, bool $disponible) 
+    {
+
+        #SE LES DA VALOR A LOS ATRIBUTOS CON LOS PARAMETROS QUE RECIBE EL METODO CONSTRUCTOR
+        $this-> $nombre; 
+        $this-> $precio;
+        $this-> $disponible;
+    }
 
     
 }
