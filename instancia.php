b0dd5f33fd97b6c14e5a8da962ed3b6d865cda17
marino5710 <brianmarin4@gmail.com>
2023-05-04 10:22:38 -0600

    Se agrega metodo constructor

--- instancia.php
+++ instancia.php
@@ -6,8 +6,20 @@ class Producto {
     public int $precio; 
     public bool $disponible; 
 
-    #metodo constructor, este se ejecuta al efectuar la instancia
-    
+     #metodo constructor, este se ejecuta al efectuar la instancia
+    public function __construct(string $nombre, int $precio, bool $disponible)
+    {
+        #SE LES DA VALORES A LOS ATRIBUTOS CON LOS PARAMETROS QUE RECIBE EL METODO CONSTRUCTOR
+        this->nombre = $nombre;
+        this->precio = $precio;
+        this->disponible = $disponible;
+
+    }
+
+        #DEFINICION DE METODOS
+        public function mostrarProducto(){
+            echo "El Producto es: " . $this->nombre . " y su precio es de: " . $this->precio; 
+        }
 }
 
 
